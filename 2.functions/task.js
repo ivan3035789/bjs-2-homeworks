f57ca502c0c0e15dee79cabca96054ff862240d9
ivan3035789 <ivan3035789@gmail.com>
2021-10-06 14:59:56 +0300

    first commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,74 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min,max,sum,avg;
+    min = Math.min(...arr);
+    max = Math.max(...arr);
+    sum = 0;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    } 
+    else if (min > arr[i]) {
+      min = arr[i];
+    }
+    sum += arr[i]
+  }
+  avg = sum / arr.length
+  return { min : min, max : max, avg : +avg.toFixed(2) };
 }
 
-// Задание 2
-function worker(arr) {
-  let sum;
+// // Задание 2
+const worker = function(arr) {
+  let maxWorker;
+  let sum = 0;
 
-  // Ваш код
-
-  return sum;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    maxWorker = Math.max(arr[i]);
+    if (sum > maxWorker){
+      maxWorker = sum;
+    }
+    else {
+      maxWorker = maxWorker;
+    }
+  }
+  return maxWorker; 
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
+function makeWork(arrayOfArrays, func) {
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+  for (let i = 0; i < arrayOfArrays.length; i++) {
+    let result = func(arrayOfArrays[i]);
+    if (result > max){
+      max = result;
+    }
+    else {
+      max = max
+    }
+  }
+ return max;
 }
 
-// Задание 3
+// // Задание 3
+
 function worker2(arr) {
-  // Ваш код
+  let maxWorker2 = -Infinity;
+  let minWorker2 = Infinity;
+
+  for (let item in arr){
+    if (maxWorker2 < item){
+      maxWorker2 = arr[item];
+    }
+    if (item < minWorker2) {
+      minWorker2 = arr[item];
+    }
+    else {
+      maxWorker2 = maxWorker2;
+      minWorker2 = minWorker2;
+    }
+  }
+  let difference = Math.abs(maxWorker2 - minWorker2);
+  return difference;
 }
